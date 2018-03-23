1fc138a02ae16eaef743e70ecb4dc2ac6664c01c
Ian Vlahovic <banddorkfht7@gmail.com>
2018-03-23 20:49:26 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,3 +12,4 @@ create_project_hash
 # title - project.css("h2.bbcard_name strong a").text
 # img link - project.css("div.project-thumbnail a img").attribute("src").value
 # description - project.css("p.bbcard_blurb").text
+project.css("ul.project-meta li a data-location").text
